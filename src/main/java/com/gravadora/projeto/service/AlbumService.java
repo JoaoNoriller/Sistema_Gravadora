c926a5ff40c4f6b92cac4019177157890dcec6a4
joaovnoriller <joaovnoriller@gmail.com>
2025-12-01 21:46:21 -0300

    Feat: Implementando a camada service

--- src/main/java/com/gravadora/projeto/service/AlbumService.java
+++ src/main/java/com/gravadora/projeto/service/AlbumService.java
@@ -1,5 +1,7 @@
-package com.gravadora.projeto.service;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class AlbumService {
-    
+
 }
--- src/main/java/com/gravadora/projeto/service/GravadoraService.java
+++ src/main/java/com/gravadora/projeto/service/GravadoraService.java
@@ -1,5 +1,44 @@
 package com.gravadora.projeto.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.gravadora.projeto.model.Gravadora;
+import com.gravadora.projeto.repository.GravadoraRepository;
+
+@Service
 public class GravadoraService {
-    
+
+    private final GravadoraRepository gravadoraRepository;
+
+    public GravadoraService(GravadoraRepository gravadoraRepository) {
+        this.gravadoraRepository = gravadoraRepository;
+    }
+
+    // SALVAR
+    public Gravadora salvar(Gravadora gravadora) {
+        return gravadoraRepository.save(gravadora);
+    }
+
+    // LISTAR
+    public List<Gravadora> listarTodos() {
+        return gravadoraRepository.findAll();
+    }
+
+    // BUSCAR POR ID
+    public Gravadora buscarPorId(Long id) {
+        return gravadoraRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Gravadora não encontrada."));
+    }
+
+    // EXCLUIR POR ID
+    public void excluir(Long id) {
+        gravadoraRepository.deleteById(id);
+    }
+
+    // EXCLUIR TODOS
+    public void excluirTodos() {
+        gravadoraRepository.deleteAll();
+    }
 }
