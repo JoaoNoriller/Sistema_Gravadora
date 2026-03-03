c30133ccb20d70409d702c5a041d2caabf2e181a
joaovnoriller <joaovnoriller@gmail.com>
2026-03-03 19:15:19 -0300

    Feat: Teste integração finalizado

--- src/test/java/com/gravadora/projeto/service/serviceIntegracao/GravadoraServiceIntegrationTest.java
+++ src/test/java/com/gravadora/projeto/service/serviceIntegracao/GravadoraServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.gravadora.projeto.service.serviceIntegracao;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -43,8 +44,99 @@ class GravadoraServiceIntegrationTest {
 
         Gravadora salva = gravadoraService.salvar(gravadora);
 
-        assertNotNull(salva.getIdGravadora()); // ou getCdGravadora se for outro nome
+        assertNotNull(salva.getIdGravadora());
         assertEquals("Universal Music", salva.getDcNome());
     }
-    
+
+    @Test
+    public void deveListarTodas() {
+
+        Gravadora gravadora1 = new Gravadora();
+        gravadora1.setDcNome("Sony");
+        gravadora1.setDcPais("EUA");
+        gravadora1.setDtDataFundacao(LocalDate.of(2000, 1, 1));
+        gravadora1.setDcTelefone("11999999999");
+        gravadora1.setDcEndereco("Rua das Flores, 100");
+        gravadora1.setDcCnpj("12345678000100");
+
+        Gravadora gravadora2 = new Gravadora();
+        gravadora2.setDcNome("Warner");
+        gravadora2.setDcPais("Brasil");
+        gravadora2.setDtDataFundacao(LocalDate.of(1990, 1, 1));
+        gravadora2.setDcTelefone("11999999988");
+        gravadora2.setDcEndereco("Rua floresta, 80");
+        gravadora2.setDcCnpj("12345678000109");
+
+        gravadoraService.salvar(gravadora1);
+        gravadoraService.salvar(gravadora2);
+
+        List<Gravadora> lista = gravadoraService.listarTodos();
+
+        assertEquals(2, lista.size());
+    }
+
+    @Test
+    public void deveBuscarPorId() {
+
+        Gravadora gravadora = new Gravadora();
+        gravadora.setDcNome("Warner");
+        gravadora.setDcPais("Brasil");
+        gravadora.setDtDataFundacao(LocalDate.of(1990, 1, 1));
+        gravadora.setDcTelefone("11999999988");
+        gravadora.setDcEndereco("Rua floresta, 80");
+        gravadora.setDcCnpj("12345678000109");
+
+        Gravadora salvo = gravadoraService.salvar(gravadora);
+
+        Gravadora encontrado = gravadoraService.buscarPorId(salvo.getIdGravadora());
+
+        assertNotNull(encontrado);
+        assertEquals("Warner", encontrado.getDcNome());
+    }
+
+    @Test
+    public void deveExcluirGravadora() {
+
+        Gravadora gravadora = new Gravadora();
+        gravadora.setDcNome("Sony");
+        gravadora.setDcPais("EUA");
+        gravadora.setDtDataFundacao(LocalDate.of(2000, 1, 1));
+        gravadora.setDcTelefone("11999999999");
+        gravadora.setDcEndereco("Rua das Flores, 100");
+        gravadora.setDcCnpj("12345678000100");
+
+        Gravadora salvo = gravadoraService.salvar(gravadora);
+
+        gravadoraService.excluir(salvo.getIdGravadora());
+
+        assertEquals(0, gravadoraService.listarTodos().size());
+    }
+
+    @Test
+    public void deveExcluirTodos() {
+
+        Gravadora gravadora1 = new Gravadora();
+        gravadora1.setDcNome("Sony");
+        gravadora1.setDcPais("EUA");
+        gravadora1.setDtDataFundacao(LocalDate.of(2000, 1, 1));
+        gravadora1.setDcTelefone("11999999999");
+        gravadora1.setDcEndereco("Rua das Flores, 100");
+        gravadora1.setDcCnpj("12345678000100");
+
+        Gravadora gravadora2 = new Gravadora();
+        gravadora2.setDcNome("Warner");
+        gravadora2.setDcPais("Brasil");
+        gravadora2.setDtDataFundacao(LocalDate.of(1990, 1, 1));
+        gravadora2.setDcTelefone("11999999988");
+        gravadora2.setDcEndereco("Rua floresta, 80");
+        gravadora2.setDcCnpj("12345678000109");
+
+        gravadoraService.salvar(gravadora1);
+        gravadoraService.salvar(gravadora2);
+
+        gravadoraService.excluirTodos();
+
+        assertEquals(0, gravadoraService.listarTodos().size());
+    }
+
 }
